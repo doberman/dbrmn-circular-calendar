6d64a4213c2ae4523552119616a35022068c31dc
Lovis Dahl <lovi.dahl@gmail.com>
2022-10-27 16:56:21 +0200

    deploy in europe

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
-const functions = require('firebase-functions')
+import functions = require( "firebase-functions" );
+
+import type {Response, https} from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+export const helloEurope = functions.region("europe-west1").https.onRequest(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (req: https.Request, response: Response<any>) => {
+      functions.logger.info("Hello logs!", {structuredData: true});
+      response.send("Hello from Firebase!");
+    }
+);
