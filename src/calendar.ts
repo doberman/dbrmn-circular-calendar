79302b47d896aead852ac20841ee15b38e2fc61a
Jonas Beck <jonas.beck@doberman.se>
2022-10-31 14:14:36 +0100

    use session storage to cache calendar data

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,18 +1,25 @@
-const API_KEY = process.env.GOOGLE_CALENDAR_API_KEY
+import { setCalendarData, getCalendarData } from './state'
 
 export const fetchCalendarData = async (year: number) => {
-  //https://developers.google.com/calendar/api/v3/reference/events/list
-  const url =
-    'https://europe-west1-dbrmn-circular-calendar.cloudfunctions.net/events'
+  let cache = getCalendarData()
 
-  console.log(url)
+  if (cache) {
+    return cache
+  } else {
+    //https://developers.google.com/calendar/api/v3/reference/events/list
+    const url =
+      'https://europe-west1-dbrmn-circular-calendar.cloudfunctions.net/events'
 
-  const response = await fetch(url)
-  if (!response.ok) {
-    console.log`An error has occured: ${response.status}`
-    return null
+    console.log(url)
+
+    const response = await fetch(url)
+    if (!response.ok) {
+      console.log`An error has occured: ${response.status}`
+      return null
+    }
+    const data = await response.json()
+    console.log(data)
+    setCalendarData(data)
+    return data
   }
-  const data = await response.json()
-  console.log(data)
-  return data
 }
